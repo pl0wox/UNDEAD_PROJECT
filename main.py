067569dfc3867373041e374660bf2a8cc7ec53f4
jetoro <76423923+pl0wox@users.noreply.github.com>
2023-05-15 11:29:27 +0800

    Hi

--- main.py
+++ main.py
@@ -33,40 +33,48 @@ class Undead:  # Parent Class
 
 class Zombie(Undead):
     zombie_list = []
-    
-    def __init__(self, name=None, hp=None):
-        super().__init__(name, hp)
-        if name is not None and hp is not None:
-            self.__hp = hp
-            self.__name = "Zombie " + name
-        else:
-            self.__hp = 100
-            self.__name = "Zombie"
-            self.__isDead = False
-            
-    # def __init__(self):
-    #     super().__init__(name, hp)
-    #     if name is not None:
-    #         self.__name = name + " Zombie"
-    #     else:
-    #         self.__name = "Zombie"
+
+    def __init__(self):
+        super().__init__()
 
     def attack(self):
         return super().getHP() * 0.5
 
+    @staticmethod
+    def add():
+        zombie = Zombie()
+        zombie_name = input("Name of the Zombie (press enter for default name): ")
+        if len(zombie_name) == 0:   # if zombie name is default
+            zombie.setName("Zombie")
+        else:                       # else not default
+            zombie.setName(zombie_name + " Zombie")
+
+        Zombie.zombie_list.append(zombie)  # add to zombie_list list
+        print(zombie.getName(), zombie.getHP())
 
 
 class Vampire(Undead):
-    def __init__(self, name=None, hp=None):
-        super().__init__(name, hp)
-
     vampire_list = []
 
-    @classmethod
-    def add(cls, name=None, hp=None):
-        new_vampire = cls(name=name, hp=hp)
-        cls.vampire_list.append(new_vampire)
-        return new_vampire
+    def __init__(self):
+        super().__init__()
+
+    def attack(self):
+        return super().getHP()
+
+    @staticmethod
+    def add():
+        vampire = Vampire()
+        vampire_name = input("Name of the Vampire (press enter for default name): ")
+        if len(vampire_name) == 0:
+            vampire.setName("Vampire")
+            vampire.setHP(120)
+        else:
+            vampire.setName(vampire_name + " Vampire")
+            vampire.setHP(120)
+
+        vampire.vampire_list.append(vampire)  # add to zombie_list list
+        print(vampire.getName(), vampire.getHP())
 
 
 class Skeleton(Undead):
@@ -75,12 +83,17 @@ class Skeleton(Undead):
 
     skeleton_list = []
 
-    @classmethod
-    def add(cls, name=None, hp=None):
-        new_skeleton = cls(name=name, hp=hp)
-        cls.skeleton_list.append(new_skeleton)
-        return new_skeleton
+    @staticmethod
+    def add():
+        skeleton = Skeleton()
+        skeleton_name = input("Name of the Skeleton (press enter for default name): ")
+        if len(skeleton_name) == 0:
+            skeleton.setName("Skeleton")
+        else:
+            skeleton.setName(skeleton_name + " Skeleton")
 
+        skeleton.skeleton_list.append(skeleton)  # add to skeleton_list list
+        print(skeleton.getName(), skeleton.getHP())
 
 
 class Ghost(Undead):
@@ -89,11 +102,17 @@ class Ghost(Undead):
 
     ghost_list = []
 
-    @classmethod
-    def add(cls, name=None, hp=None):
-        new_ghost = cls(name=name, hp=hp)
-        cls.ghost_list.append(new_ghost)
-        return new_ghost
+    @staticmethod
+    def add():
+        ghost = Ghost()
+        ghost_name = input("Name of the Ghost (press enter for default name): ")
+        if len(ghost_name) == 0:
+            ghost.setName("Ghost")
+        else:
+            ghost.setName(ghost_name + " Ghost")
+
+        Ghost.ghost_list.append(ghost)  # add to ghost_list list
+        print(ghost.getName(), ghost.getHP())
 
 
 class Lich(Undead):
@@ -102,11 +121,17 @@ class Lich(Undead):
 
     lich_list = []
 
-    @classmethod
-    def add(cls, name=None, hp=None):
-        new_lich = cls(name=name, hp=hp)
-        cls.lich_list.append(new_lich)
-        return new_lich
+    @staticmethod
+    def add():
+        lich = Lich()
+        lich_name = input("Name of the Lich (press enter for default name): ")
+        if len(lich_name) == 0:
+            lich.setName("Lich")
+        else:
+            lich.setName(lich_name + " Lich")
+
+        Lich.lich_list.append(lich)  # add to lich_list list
+        print(lich.getName(), lich.getHP())
 
 
 class Mummy(Undead):
@@ -115,16 +140,24 @@ class Mummy(Undead):
 
     mummy_list = []
 
-    @classmethod
-    def add(cls, name=None, hp=None):
-        new_mummy = cls(name=name, hp=hp)
-        cls.mummy_list.append(new_mummy)
-        return new_mummy
+    @staticmethod
+    def add():
+        mummy = Mummy()
+        mummy_name = input("Name of the Mummy (press enter for default name): ")
+        if len(mummy_name) == 0:
+            mummy.setName("Mummy")
+        else:
+            mummy.setName(mummy_name + " Mummy")
+
+        Mummy.mummy_list.append(mummy)  # add to mummy_list list
+        print(mummy.getName(), mummy.getHP())
 
 
 def main_menu():  # main menu
-    print("""          
-U N D E A D
+    print(""" 
+  -=============-        
+  + U N D E A D +
+  -=============-
 
 [1] - Create Undead
 [2] - Command Undead
@@ -137,26 +170,112 @@ U N D E A D
         case 2:
             return choose_command()
         case 3:
-            return "Display"
+            return display_all()
         case _:
             return 0
 
 
 def choose_command():
-    undead_counter = int(1)
-    print("\n\n C H O O S E  U N D E A D\n\n")
-    if not len(Zombie.zombie_list):
-        print("-= Z O M B I E S =-")
-        print("[No Zombies Created]")
+    print("-=================================-")
+    print("    C O M M A N D  U N D E A D     ")
+    print("-=================================-")
+    print("[A] - Zombie")
+    print("[B] - Vampire")
+    print("[C] - Skeleton")
+    print("[D] - Ghost")
+    print("[E] - Lich")
+    print("[F] - Mummy")
+    print("[0] <-- Go back")
+
+    match choice():
+        case 'a':
+            return select_zombie()
+        case 'b':
+            return select_vampire()
+        case _:
+            return choose_command()
+
+
+def select_zombie():
+    counter = 1
+    if len(Zombie.zombie_list) == 0:
+        print("\nNo Zombies.\n\n")
+        return choose_command()
     else:
-        print("-= Z O M B I E S =-")
-        for show_zombies in Zombie.zombie_list:
-            print("[", undead_counter, "]", Zombie.getName(show_zombies))
-            undead_counter += 1
+        print("\n-=======================-")
+        print("      Z O M B I E S      ")
+        print("-=======================-")
+
+        for i, zombie in enumerate(Zombie.zombie_list):
+            print("[", counter, "]", zombie.getName())
+            counter += 1
+
+    x = int(input("Select a zombie: "))
+
+    if 1 <= x <= len(Zombie.zombie_list):
+        selected_zombie = Zombie.zombie_list[x - 1]
+        print("You selected:", selected_zombie.getName())
+        # Perform actions with the selected zombie here
+
+        print("                         [SKILLS]\n")
+        print("[1] - Eat")
+        print("Eat another undead as a result it will increase its HP by the")
+        print("half of the HP of the undead being eaten\n")
+        print("[2] - Normal Attack")
+        print("Deals is half of its HP.\n")
+
+        match int(choice()):
+            case 1:
+                print("Use Bite to who?\n\n")
+                attack_undead(selected_zombie.getName())
+
+                return
+            case 2:
+                print("YOu have used normal attack!!")
+                return main_menu()
+
+    else:
+        print("Invalid selection. Please try again.")
+        select_zombie()
+
+
+        def bite():
+            find_undead = (Zombie.zombie_list + Vampire.vampire_list)
+
+    return choose_command()
+
+
+def select_vampire():
+    counter = 1
+    if len(Vampire.vampire_list) == 0:
+        print("\nThere are No Vampires. try again.")
+        return choose_command()
+    else:
+        print("\n\n-=======================-")
+        print("     V A M P I R E     ")
+        print("-=======================-")
+        for i, vampire in enumerate(Vampire.vampire_list):
+            print("[", counter, "]", vampire.getName())
+            counter += 1
+
+    x = int(input("Select a vampire: "))
+
+    if 1 <= x <= len(Zombie.zombie_list):
+        selected_zombie = Zombie.zombie_list[x - 1]
+        print("You selected:", selected_zombie.getName())
+        # Perform actions with the selected zombie here
+    else:
+        print("Invalid selection. Please try again.")
+        select_zombie()
+
+    return choose_command()
 
 
 def choose_undead():
-    print("\n\n C R E A T E  U N D E A D")
+    print("\n-=======================-")
+    print(" C R E A T E  U N D E A D")
+    print("-=======================-")
+
     print("[A] - Zombie")
     print("[B] - Vampire")
     print("[C] - Skeleton")
@@ -167,60 +286,96 @@ def choose_undead():
 
     match choice():
         case 'a':  # Zombie
-            zombie = Zombie()
-            zombie_name = input("Name of the Zombie (press enter for default name): ")
-            zombie.setName(zombie_name)
-
-            Zombie.zombie_list.append(zombie)   # add to zombie_list list
-            print(zombie.getName(), zombie.getHP())
+            Zombie.add()
             return main_menu()
 
         case 'b':  # Vampire
-            vampire_name = input("Name of the Vampire (press enter for default name): ")
-            if len(vampire_name) == 0:
-                Vampire.add("Vampire", int(120))
-            else:
-                Vampire.add(vampire_name + " Vampire", int(120))
-
-            for vampire in Vampire.vampire_list:
-                print(Vampire.getHP(vampire), Vampire.getName(vampire))
+            Vampire.add()
             return main_menu()
 
         case 'c':  # Skeleton
-            skeleton_name = input("Name of the Skeleton (press enter for default name): ")
-            if len(skeleton_name) == 0:
-                Skeleton.add("Skeleton", int(80))
-            else:
-                Skeleton.add(skeleton_name + " Skeleton", int(80))
+            Skeleton.add()
             return main_menu()
 
         case 'd':  # Ghost
-            ghost_name = input("Name of the Ghost (press enter for default name): ")
-            if len(ghost_name) == 0:
-                Ghost.add("Ghost", (100 / 2))
-            else:
-                Ghost.add(ghost_name + " Ghost", int(80))
+            Ghost.add()
             return main_menu()
 
         case 'e':  # Lich
-            lich_name = input("Name of the Lich (press enter for default name): ")
-            if len(lich_name) == 0:
-                Lich.add("Lich", int(80))
-            else:
-                Lich.add(lich_name + " Lich", int(80))
+            Lich.add()
             return main_menu()
 
         case 'f':  # Mummy
-            mummy_name = input("Name of the Mummy (press enter for default name): ")
-            if len(mummy_name) == 0:
-                Mummy.add("Mummy", int(100))
-            else:
-                Mummy.add(mummy_name + " Mummy", int(100))
+            Mummy.add()
             return main_menu()
 
         case _:
             print("Invalid Input!!!\n\n")
             return choose_undead()
+
+
+def display_all():
+    undead_list = (Zombie.zombie_list + Vampire.vampire_list + Skeleton.skeleton_list + Ghost.ghost_list +
+                   Lich.lich_list + Mummy.mummy_list)
+
+    if len(undead_list) == 0:
+        print("No Undead created.")
+        return main_menu()
+
+    for undead in undead_list:
+        print("--------------------")
+        print(undead.getName())
+        print("HP: ", undead.getHP())
+        print("State: ", undead.isDead())
+    return main_menu()
+
+
+def attack_undead(select_undead):
+    print("-=================================-")
+    print("    A T T A C K   U N D E A D     ")
+    print("-=================================-")
+    print("[A] - Zombie")
+    print("[B] - Vampire")
+    print("[3] - Skeleton")
+    print("[4] - Ghost")
+    print("[5] - Lich")
+    print("[6] - Mummy")
+    print("[0] <-- Go back")
+
+    match choice():
+        case 'a':
+            counter = 1
+            for z, zombie in enumerate(Zombie.zombie_list):
+                print("[", counter, "]", zombie.getName())
+                counter += 1
+
+            x = int(input("Select a zombie: "))
+
+            if 1 <= x <= len(Zombie.zombie_list):
+                selected_zombie = Zombie.zombie_list[x - 1]
+                print(select_undead, " will bite ", selected_zombie.getName())
+                # Perform actions with the selected zombie here
+            else:
+                print("Invalid selection. Please try again.")
+                select_zombie()
+            return main_menu()
+
+        case 'b':
+            counter = 1
+            for v, vampire in enumerate(Vampire.vampire_list):
+                print("[", counter, "]", vampire.getName())
+                counter += 1
+
+            x = int(input("Select a Vampire: "))
+
+            if 1 <= x <= len(Vampire.vampire_list):
+                selected_vampire = Vampire.vampire_list[x - 1]
+                print(select_undead, " will bite ", selected_vampire.getName())
+                # Perform actions with the selected zombie here
+            else:
+                print("Invalid selection. Please try again.")
+                select_vampire()
+            return main_menu()
 
 
 def choice():
